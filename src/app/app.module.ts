5ffd3d2f2fe42cde74c5dde26734fcf95ea64b99
R. Kaleta <rafkaleta@onet.pl>
2019-07-15 17:43:49 +0200

    Components Board and Field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { FieldComponent } from './field/field.component';
+import { NormalBoardComponent } from './normal-board/normal-board.component';
+import { NormalModeComponent } from './normal-mode/normal-mode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    FieldComponent,
+    NormalBoardComponent,
+    NormalModeComponent
   ],
   imports: [
     BrowserModule,
