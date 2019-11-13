fe68e3637b3cac31edd7913d4e8898b29c5fdcfe
R. Kaleta <rafkaleta@onet.pl>
2019-11-13 23:07:41 +0100

    Small formatting

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,21 +1,21 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging } from "protractor";
+import { AppPage } from "./app.po";
 
-describe('workspace-project App', () => {
+describe("workspace-project App", () => {
     let page: AppPage;
 
     beforeEach(() => {
         page = new AppPage();
     });
 
-    it('should display welcome message', () => {
+    it("should display welcome message", () => {
         page.navigateTo();
-        expect(page.getTitleText()).toEqual('Welcome to ZenSaper!');
+        expect(page.getTitleText()).toEqual("Welcome to ZenSaper!");
     });
 
     afterEach(async () => {
         // Assert that there are no errors emitted from the browser
-        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        const logs: logging.Entry[] = await browser.manage().logs().get(logging.Type.BROWSER);
         expect(logs).not.toContain(jasmine.objectContaining({
             level: logging.Level.SEVERE,
         } as logging.Entry));
