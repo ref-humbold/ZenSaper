09aa5ffb6e4a351b937349775ecf35978c70f020
R. Kaleta <rafkaleta@onet.pl>
2019-12-11 22:15:33 +0100

    Remove unused routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FieldComponent } from "./components/field/field.component";
 import { GameBoardComponent } from "./components/game-board/game-board.component";
@@ -11,7 +10,7 @@ import { GameBoardComponent } from "./components/game-board/game-board.component
         FieldComponent,
         GameBoardComponent
     ],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent]
 })
